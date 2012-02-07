8d99d236cfd97727dd6e0666840bbe021da5d4c1
Paolo D'Apice <dapicester@gmail.com>
2012-02-07 21:10:38 +0800

    added user model

---
+++ app/models/user.rb
@@ -0,0 +1,26 @@
+class User < ActiveRecord::Base
+
+  SCREEN_NAME_MIN_LENGTH = 5
+  SCREEN_NAME_MAX_LENGTH = 50
+  SCREEN_NAME_RANGE = SCREEN_NAME_MIN_LENGTH..SCREEN_NAME_MAX_LENGTH  
+
+  PASSWORD_MIN_LENGTH = 4
+  PASSWORD_MAX_LENGTH = 50
+  PASSWORD_RANGE = PASSWORD_MIN_LENGTH..PASSWORD_MAX_LENGTH
+
+  EMAIL_MAX_LENGTH = 50
+
+  SCREEN_NAME_SIZE = 20
+  PASSWORD_SIZE = 10
+  EMAIL_SIZE = 30
+  
+  validates :screen_name, :uniqueness => true,
+                          :length => { :within => SCREEN_NAME_RANGE },
+                          :format => { :with => /^[A-Z0-9_]*$/i, :message => "must contains only letters, numbers and underscores" }
+  validates :email, :presence => true,
+                    :uniqueness => true , 
+                    :length => { :maximum => EMAIL_MAX_LENGTH },
+                    :format => { :with => /^[^@][\w.-]+@[\w.-]+[.][a-z]{2,4}$/i, :message => "must be a valid address" }
+
+  validates :password, :length => { :within => PASSWORD_RANGE }
+end
