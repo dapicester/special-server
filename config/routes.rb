9c2f4db6a1ef5564583b98f2800adb61192233fd
Paolo D'Apice <dapicester@gmail.com>
2012-02-07 20:41:01 +0800

    added site controller
    site actions:
    - index (default)
    - about
    - help
    
    TODO: links in navigation bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 SpecialServer::Application.routes.draw do
+  get "site/index"
+  get "site/about"
+  get "site/help"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +52,7 @@ SpecialServer::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'site#index'
 
   # See how all your routes lay out with "rake routes"
 
