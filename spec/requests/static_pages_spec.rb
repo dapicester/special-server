7026be1a12f6b25d5f002faa323f15bd8dda75a7
Paolo D'Apice <dapicester@gmail.com>
2012-02-16 20:43:16 +0800

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,51 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+
+  let(:base_title) { "Special" }
+
+  describe "Home page" do
+    it "should have the content 'Special'" do
+      visit '/static_pages/home'
+      page.should have_selector('h1', :text => 'Special')
+    end
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector('title', :text => "#{base_title} | Home")
+    end
+  end
+
+  describe "Help Page" do
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('h1', :text => 'Help')
+    end
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('title', :text => "#{base_title} | Help")
+    end
+  end
+
+  describe "About page" do
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'About Us')
+    end
+    it "should have the title 'About'" do
+      visit '/static_pages/about'
+      page.should have_selector('title', :text => "#{base_title} | About")
+    end 
+  end
+
+  describe "Contact page" do
+    it "should have the content 'Contact Special'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', :text => "#{base_title} | Contact")
+    end
+  end
+
+end
