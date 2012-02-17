61ed37325f5f786124151dfa8c50a593fb5e5965
Paolo D'Apice <paolo.dapice@izenesoft.com>
2012-02-17 11:04:48 +0800

    Added password to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,17 @@
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime        not null
-#  updated_at :datetime        not null
+#  id              :integer         not null, primary key
+#  name            :string(255)
+#  email           :string(255)
+#  created_at      :datetime        not null
+#  updated_at      :datetime        not null
+#  password_digest :string(255)
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :name, :email
+  attr_accessible :name, :email, :password, :password_confirmation
+  has_secure_password
 
   validates :name, :presence => true,
                    :length => { :maximum => 50 }
@@ -19,4 +21,6 @@ class User < ActiveRecord::Base
   validates :email, :presence => true,
                     :format => { :with => valid_email_regex },
                     :uniqueness => { :case_sensitive => false }
+
+  validates :password, :length => { :minimum => 6 }
 end
