84a410ea6b9e902ce620dc6c88775d3e0c8db1d9
Paolo D'Apice <paolo.dapice@izenesoft.com>
2012-02-17 09:38:25 +0800

    Refactoring logo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,10 @@
 module ApplicationHelper
 
+  # Returns the logo
+  def logo
+    logo = image_tag("logo.png", :alt => "Logo", :class => "round")
+  end
+
   # Returns the full title on a per-page basis
   def full_title(page_title)
     base_title = "Special Social Network"
