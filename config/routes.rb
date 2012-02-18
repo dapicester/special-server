7b9111af25da20a9513bce35885c9ef8ab7bfd66
Paolo D'Apice <dapicester@gmail.com>
2012-02-18 15:10:19 +0800

    Added signin

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 SpecialServer::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
-  match "/signup",  to: "users#new"
-  match "/help",    to: "static_pages#help"
-  match "/about",   to: "static_pages#about"
-  match "/contact", to: "static_pages#contact"
+  match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy'
+
+  match '/help',    to: 'static_pages#help'
+  match '/about',   to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
 
   root to: "static_pages#home"
 
