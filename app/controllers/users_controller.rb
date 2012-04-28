c70a94a3095e2b2837ef758f4020963d8db5f9be
Paolo D'Apice <paolo.dapice@izenesoft.com>
2012-04-28 15:39:23 +0800

    Added localization for user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     if @user.save
       sign_in @user
-      flash[:success] = "Welcome to Special!"
+      flash[:success] = I18n.t('users.create.success')
       redirect_to @user
     else
       render 'new'
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
 
   def update
     if @user.update_attributes(params[:user])
-      flash[:success] = "Profile updated"
+      flash[:success] = I18n.t('users.update.success')
       sign_in @user
       redirect_to @user
     else
@@ -45,19 +45,19 @@ class UsersController < ApplicationController
 
   def destroy
     User.find(params[:id]).destroy
-    flash[:success] = "User deleted."
+    flash[:success] = I18n.t('users.destroy.success')
     redirect_to users_path
   end
 
   def following
-    @title = "Following"
+    @title = I18n.t('users.following.title')
     @user = User.find(params[:id])
     @users = @user.followed_users.paginate(page: params[:page])
     render 'show_follow'
   end
 
   def followers
-    @title = "Followers"
+    @title = I18n.t('users.followers.title')
     @user = User.find(params[:id])
     @users = @user.followers.paginate(page: params[:page])
     render 'show_follow'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "Special Workplaces"
+    base_title = t('appname')
     if page_title.empty?
       base_title
     else
