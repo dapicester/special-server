be7a0654cda152a0bc5d2d0de3a11d8a04fccad5
Paolo D'Apice <dapicester@gmail.com>
2013-05-27 20:29:24 +0800

    Updated models annotations

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,11 +2,11 @@
 #
 # Table name: microposts
 #
-#  id         :integer         not null, primary key
+#  id         :integer          not null, primary key
 #  content    :string(255)
 #  user_id    :integer
-#  created_at :datetime        not null
-#  updated_at :datetime        not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
 #
 
 class Micropost < ActiveRecord::Base
--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -2,11 +2,11 @@
 #
 # Table name: relationships
 #
-#  id          :integer         not null, primary key
+#  id          :integer          not null, primary key
 #  follower_id :integer
 #  followed_id :integer
-#  created_at  :datetime        not null
-#  updated_at  :datetime        not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
 #
 
 require 'spec_helper'
