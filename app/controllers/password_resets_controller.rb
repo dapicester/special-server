fbffaa27ba65a3c35ccb3f76e6439a5f0d489895
Paolo D'Apice <dapicester@gmail.com>
2013-05-29 22:57:49 +0800

    Complete reset password

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -26,9 +26,11 @@ class PasswordResetsController < ApplicationController
   def update
     @user = User.find_by_password_reset_token! params[:id]
     if @user.password_reset_sent_at < 2.hours.ago
-      redirect_to new_passsword_reset_path, alert: "Password &crarr; reset has espired"
+      flash[:error] = "Your password reset request has espired."
+      redirect_to new_password_reset_path
     elsif @user.update_attributes params[:user]
-      redirect_to root_url, notice: 'Password has been reset.'
+      flash[:success] = "Password has been changed."
+      redirect_to signin_path
     else
       render :edit
     end
--- spec/requests/password_resets_spec.rb
+++ spec/requests/password_resets_spec.rb
@@ -30,6 +30,7 @@ describe PasswordResetsController do
       click_button 'Reset'
       page.should have_content "Email sent"
       last_email.should be_nil # no email sent even if we say so
+      page.should have_selector('title', text: 'Sign in')
     end
   end
 
@@ -39,10 +40,38 @@ describe PasswordResetsController do
       click_button 'Reset'
       page.should have_content "Email sent"
       last_email.to.should include(user.email)
+      page.should have_selector('title', text: 'Sign in')
+    end
+
+    describe "when changing the password" do
+      before do
+        user.password_reset_token = 'token_value'
+        user.password_reset_sent_at = Time.now
+        user.save
+        visit edit_password_reset_path(user.password_reset_token)
+      end
+      it "should change the password" do
+        # TODO common spec for concern
+        fill_in "Password", with: "newfoobar"
+        fill_in "Password confirmation", with: "newfoobar"
+        click_button 'Change password'
+        page.should have_message :success, 'Password has been changed.'
+        page.should have_selector('title', text: 'Sign in')
+      end
+      describe "if the token is expired" do
+        before do
+          user.password_reset_sent_at = 125.minutes.ago
+          user.save
+        end
+        it "should not change the password" do
+          #TODO helpers for fill-in and click
+          fill_in "Password", with: "newfoobar"
+          fill_in "Password confirmation", with: "newfoobar"
+          click_button 'Change password'
+          page.should have_message :error, 'Your password reset request has espired.'
+          page.should have_selector 'title', text: 'Reset Password'
+        end
+      end
     end
   end
-
-  # TODO: password changes on confirmation
-  # TODO: token expired
-  # TODO: token invalid
 end
