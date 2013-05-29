de54e76a7be2fb06fbbd1a86cf0c23ab9d4d3033
Paolo D'Apice <dapicester@gmail.com>
2013-05-29 23:10:11 +0800

    Removed Chinese localization (configuration only)

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,7 @@ module SpecialServer
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
-    config.i18n.available_locales = [:en, :it, "zh-CN"]
+    config.i18n.available_locales = [:en, :it]
     # config.i18n.default_locale = :de
 
     # Configure the default encoding used in templates for Ruby 1.9.
