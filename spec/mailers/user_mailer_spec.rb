c08edab4e54349076df414f53a92a1fe6f4cbb9c
Paolo D'Apice <dapicester@gmail.com>
2013-05-31 00:02:27 +0800

    Added mail translations

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -6,13 +6,15 @@ describe UserMailer do
     let(:mail) { UserMailer.password_reset(user) }
 
     it "renders the headers" do
-      mail.subject.should eq("Password Reset")
+      mail.subject.should eq t('user_mailer.password_reset.subject')
       mail.to.should eq([user.email])
       mail.from.should eq(["from@example.com"])
     end
 
     it "renders the body" do
+      mail.body.encoded.should match(t 'user_mailer.password_reset.body.link')
       mail.body.encoded.should match(edit_password_reset_path(user.password_reset_token))
+      mail.body.encoded.should match(t 'user_mailer.password_reset.body.ignore')
     end
   end
 
