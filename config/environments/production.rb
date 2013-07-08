4b19b7dc022e54bbacf284318315af9cafa49723
Paolo D'Apice <dapicester@gmail.com>
2013-07-08 09:55:17 +0800

    Using SendGrid for sending emails on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,8 +49,16 @@ SpecialServer::Application.configure do
   config.assets.precompile += %w( counter.js )
 
   # Set the mailer
-  # FIXME use real domain
-  #config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: 'sharp-frost-9678.herokuapp.com' }
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
