365f9aadab59de65fa423015ddebb5242d43a0f7
Paolo D'Apice <dapicester@gmail.com>
2013-07-08 10:52:31 +0800

    Using user name and email address when sending emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,13 +3,19 @@ class UserMailer < ActionMailer::Base
 
   def password_reset(user)
     @user = user
-    mail to: @user.email,
+    mail to: email_with_name,
          subject: t('user_mailer.password_reset.subject')
   end
 
   def activation(user)
     @user = user
-    mail to: @user.email,
+    mail to: email_with_name,
          subject: t('user_mailer.activation.subject')
+  end
+
+private
+
+  def email_with_name
+    "#{@user.name} <#{@user.email}>"
   end
 end
