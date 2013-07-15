f5fb586231a41a20e490fc3fdab20ebcc8dd503f
Paolo D'Apice <dapicester@gmail.com>
2013-07-15 15:36:58 +0800

    Revert "Disabling Gravatar in development"
    This reverts commit 06bab0bec7dd90befdc7eb8ce7f4750edfb0b6bd.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,10 +2,6 @@ module UsersHelper
 
   # Returns the Gravatar (http://gravatar.com/) for the given user.
   def gravatar_for(user, options = { size: 50 })
-    if Rails.env.development?
-      style = "width:#{options[:size]}px;height#{options[:size]}px;border:1px solid #000;float:left;margin-right:10px"
-      return image_tag '', alt: user.name, style: style
-    end
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
     gravatar_url = "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
