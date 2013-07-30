35cc31268f1c6b9d496b0a683f9498da18729edb
Paolo D'Apice <dapicester@gmail.com>
2013-07-30 21:19:44 +0800

    WIP Moved language-specific translations to locales/specific

--- spec/support/i18n_helper.rb
+++ spec/support/i18n_helper.rb
@@ -1,5 +1,6 @@
 # https://gist.github.com/henrik/2994129
 
+require 'i18n'
 require 'yaml'
 
 module I18nHelper
@@ -21,8 +22,10 @@ module I18nHelper
   end
 
   def base_i18n_files
-    Dir["config/locales/**/*.yml"].select do |file|
+    Dir[File.join "config", "locales", "**", "*.yml"].select do |file|
       File.basename(file).match I18n.available_locales.join('|')
+    end.select do |file|
+      not File.dirname(file).match 'specific'
     end
   end
 
@@ -33,7 +36,9 @@ module I18nHelper
   def locales_to_documents
     @locales_to_documents = locales_to_keys.keys.inject({}) do |hash, locale|
       # E.g. [ "terms" ]
-      documents = Dir["config/locales/documents/*.#{locale}.*"].map { |file| File.basename(file, ".#{locale}.markdown") }
+      documents = Dir[File.join "config", "locales", "documents", "*.#{locale}.*"].map do |file|
+        File.basename(file, ".#{locale}.markdown")
+      end
       hash.merge locale => documents
     end
   end
